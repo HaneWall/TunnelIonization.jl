dd6d3f7e47cdf04b58fe64f04f58b9867315fd3c
Documenter.jl <documenter@juliadocs.github.io>
2021-12-02 15:24:46 +0000

    build based on 359e5cf

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TunnelIonization","category":"page"},{"location":"#TunnelIonization","page":"Home","title":"TunnelIonization","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TunnelIonization.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TunnelIonization]","category":"page"}]
+}
